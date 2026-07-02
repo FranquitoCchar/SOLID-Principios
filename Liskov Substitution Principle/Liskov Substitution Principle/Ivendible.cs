b497208011a9e86eae3f289c8816d2788390651a
DanielPicadoM <66844432+DanielPicadoM@users.noreply.github.com>
2026-07-02 15:34:26 -0600

    upload de mi principio solid
    se sube principio solid de liskov, perteneciente a la exposición solicitada en clase

---
+++ Liskov Substitution Principle/Liskov Substitution Principle/Ivendible.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liskov_Substitution_Principle
+{
+    public interface Ivendible
+    {
+        void Vender(int cantidad);
+    }
+}
---
+++ Liskov Substitution Principle/Liskov Substitution Principle/ProductoDeMuestra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liskov_Substitution_Principle
+{
+    internal class ProductoDeMuestra:ProductoSinLSP
+    {
+        public override void Vender(int cantidad)
+        {
+            throw new NotImplementedException(
+                "Los productos de muestra no pueden venderse."
+            );
+        }
+    }
+}
